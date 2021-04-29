0b7b123866c4dadf1d8d65e15e800e74c149fc77
Owen Flaherty <o.flaherty@theeastbourneacademy.org>
2021-04-29 01:54:37 +0100

    Synchronised home -> git - 29/04/2021 @ 01:54

--- git2home.sh
+++ git2home.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Choose the theme by changing the variable below
+THEME="amora"
+
+# Available themes:
+#  - amora
+#  - rouge
+
 # Pull changes from git
 git pull
 
@@ -8,3 +15,10 @@ rsync -avu --delete ./Awesome/ ~/.config/awesome
 
 # Wallpapers
 rsync -avu --delete ./Papes/ ~/Pictures/Papes
+
+# Xresources
+#cp ./Xresources/$THEME.xresources ~/.Xresources
+
+# Emacs (DOOM) config
+#rsync -avu --delete ./Emacs/emacsd/ ~/.emacs.d
+#rsync -avu --delete ./Emacs/doomd/ ~/.doom.d
--- home2git.sh
+++ home2git.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# Choose the theme by changing the variable below
+THEME="amora"
+
+# Available themes:
+#  - amora
+#  - rouge
+
 # Config for awesomewm
 rsync -avu --delete ~/.config/awesome/ ./Awesome
 
 # Wallpapers
 rsync -avu --delete ~/Pictures/Papes/ ./Papes
+
+# Xresources
+#cp ~/.Xresources ./Xresources/$THEME.xresources
+
+# Emacs (DOOM) config
+#rsync -avu --delete ~/.emacs.d/ ./Emacs/emacsd
+#rsync -avu --delete ~/.doom.d/ ./Emacs/doomd
 
 # Push changes to git
 git add .
